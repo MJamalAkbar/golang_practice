ab6e36705284807e71ca6f9a33caf72439089bc8
abc <abc@abcs-MacBook-Pro.local>
2022-03-09 12:48:39 +0500

    Task1 Decleartions of datatypes

---
+++ wtask1.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main (){
+	var a int = 5
+	var b float32 = 2.5
+	var c string = "Hello-World"
+		fmt.Print("integer = ", a)
+		fmt.Print("\nFloat = ", b)
+		fmt.Print("String = ", c)
+
+}
